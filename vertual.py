19c3d5ef55babbf72d3595ca9eaf76570cea7887
YOUNESS-RZIQI <younessrziki204@gmail.com>
2026-06-05 15:15:43 +0100

    installed : uv, mypy, flake8

---
+++ vertual.py
@@ -0,0 +1,11 @@
+1) we need the vertual invironment to solve the problem of dependencies coflect.
+
+2) to creat one  : python -m venv venv
+
+3) to activate   : source virtual_env/bin/activate
+
+4) to deactivate : deactivate
+
+5) to know if it is activated use: which python3
+
+6) to see the list of installed apps do : pip list.
