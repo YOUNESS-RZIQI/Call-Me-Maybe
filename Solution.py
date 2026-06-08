e821db0fa6129fedfd09c80b6bbd8a5838e7d35f
YOUNESS-RZIQI <younessrziki204@gmail.com>
2026-06-08 16:35:01 +0100

    Update

--- Solution.py
+++ Solution.py
@@ -6,13 +6,16 @@ import json
 
 
 def is_valide_json_file(file_name: str) -> bool:
-    """Is the format of the file is JSON"""
+    """Validate a file is it a full JSON Format (True, False)"""
     try:
         with open(file_name, "r", encoding="utf-8") as file:
             json.load(file)
         return True
-    except json.JSONDecodeError:
+    except (json.JSONDecodeError, FileNotFoundError, PermissionError):
+        return False
+    except Exception as e:
+        print(f"Error in (is_valide_json_file Function)\n{e}")
         return False
 
 
-print(is_valide_json_file("tst.json"))
+print(is_valide_json_file("tssst.json"))
