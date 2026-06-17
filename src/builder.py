c5f1348476db5d67f5d7ce9b8898404c0228e47b
YOUNESS-RZIQI <younessrziki204@gmail.com>
2026-06-17 15:58:52 +0100

    update

--- src/builder.py
+++ src/builder.py
@@ -5,13 +5,13 @@ from typing import List
 class PromptBuilder:
     """ Builder Tools """
     @staticmethod
-    def build_final_prompt_string(prompts: str, definitions_list: List[DefinitionValidator]) -> str:
+    def build_final_prompt_string(prompt: str, definitions_list: List[DefinitionValidator]) -> str:
         """ Build Final Prompt String """
         final_string = ""
         i = 1
         for function in definitions_list:
             final_string += f"Function {i}: {function.name}({', '.join([f'{key}: {value.type}' for key, value in function.parameters.items()])})"
             i += 1
-        final_string += f' Request: ({prompts}), ' + \
-            'expected JSON schema: {\n"prompt": "...",\n"name": "...",\n"parameters": {"argment1": value1, ...}\n}, JSON: '
+        final_string += f' Request: ({prompt}), ' + \
+            'Result:\n{\n"prompt": "' + f'{prompt}",\n"name": "'
         return final_string
