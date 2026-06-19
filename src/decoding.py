bf356ac8ee8928c2523465812bc3c351edc1072e
YOUNESS-RZIQI <younessrziki204@gmail.com>
2026-06-19 15:54:28 +0100

    compliting functions predection using masking.

--- src/decoding.py
+++ src/decoding.py
@@ -1,11 +1,9 @@
-from enum import Enum, auto
 from llm_sdk import Small_LLM_Model
 import numpy as np
-from typing import List, Dict
+from typing import List
 import sys
 import traceback
-from src.models import DefinitionValidator
-
+import json
 
 
 def constrain_decoding(llm_prompt: str, input_prompt: str, functions_def_list: List[str]) -> str:
@@ -13,20 +11,26 @@ def constrain_decoding(llm_prompt: str, input_prompt: str, functions_def_list: L
     try:
         model: Small_LLM_Model = Small_LLM_Model()
         dict_result: str = '{\n"prompt": "' + f'{input_prompt}",\n"name": "'
-
         function_number: str = ""
+        vocab_path = model.get_path_to_vocab_file()
+        with open(vocab_path, "r") as f:
+            vc = json.load(f)
 
         while (1):
-            logits: List[float] = model.get_logits_from_input_ids(model.encode(llm_prompt + function_number)[0].tolist())
+            logits: List[float] = model.get_logits_from_input_ids(
+                model.encode(llm_prompt + function_number)[0].tolist())
+            # masking the logits to only allow digits from 0-9 and comma and dot.
+            for index in range(0, len(logits)):
+                if index not in (vc["0"], vc["1"], vc["2"], vc["3"], vc["4"], vc["5"],
+                                 vc["6"], vc["7"], vc["8"], vc["9"], vc["."], vc[","]):
+                    logits[index] = float("-inf")
+
             next_token_id = int(np.argmax(logits))
             if not (model.decode(next_token_id).isdigit()):
                 break
             function_number += model.decode(next_token_id)
-        #check that the number nout out side of the list " unknown function name"
-        if int(function_number) >= len(functions_def_list):
-            dict_result += " unknown function call"
-        else:
-            dict_result += functions_def_list[int(function_number)]
+
+        dict_result += functions_def_list[int(function_number)]
         dict_result += '",\n"parameters": {"a": '
 
         print(dict_result, "\n\n")
