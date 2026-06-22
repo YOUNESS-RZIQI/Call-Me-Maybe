f717f7f1a6e39b3adae2c01722e62a8890b0550c
YOUNESS-RZIQI <younessrziki204@gmail.com>
2026-06-22 16:25:27 +0100

    Updates

--- src/decoding.py
+++ src/decoding.py
@@ -46,7 +46,7 @@ def constrain_decoding(llm_prompt: str, input_prompt: str) -> str:
             function_number += model.decode(next_token_id)
 
         dict_result += functions_def_list[int(function_number)]
-        dict_result += '",\n"parameters": '
+        dict_result += '",\n"parameters": { '
 
         chosen_func_obj: DefinitionValidator = None
         for func in functions_def_obj:
@@ -63,14 +63,12 @@ def constrain_decoding(llm_prompt: str, input_prompt: str) -> str:
             ky_type.append(value.type)
         
         
-        llm_prompt: str += dict_result
+        llm_prompt += dict_result
 
         # i will do all the args until the last one and i will stop, and also i stop at comma ','
-        for key_index in range(0, len(keys) - 1):
-            dict_result += '"' + keys[key_index] + '" '
-            llm_prompt += '"' + keys[key_index] + '" '
-            
-            print("llm prompt:\n", llm_prompt)
+        for key_index in range(0, len(keys)):
+            dict_result += '"' + keys[key_index] + '": '
+            llm_prompt += '"' + keys[key_index] + '": '
             
             if ky_type[key_index] in ("number", "decimal", "float"):
                 allowed_tokens_ids = {
@@ -86,17 +84,37 @@ def constrain_decoding(llm_prompt: str, input_prompt: str) -> str:
 
             if ky_type[key_index] in ("bool", "boolean"):
                 allowed_tokens_ids = {vc["true"], vc["True"], vc["TRUE"],
-                                     vc["false"], vc["False"], vc["FALSE"]}
+                                     vc["false"], vc["False"], vc["FALSE"],
+                                     vc[","]}
 
             if ky_type[key_index] in ("string", "s"):
                 allowed_tokens_ids = set(range(len(vc)))
+
+
+            # '",\n"parameters": { "a": '
+            
                 
+            while (1):
+                logits: List[float] = model.get_logits_from_input_ids(
+                    model.encode(llm_prompt)[0].tolist())
+                # masking the logits to only allow the allowed_tokens_ids.
+                for index in range(0, len(logits)):
+                    if index not in allowed_tokens_ids:
+                        logits[index] = float("-inf")
+    
+                next_token_id = int(np.argmax(logits))
+                if "," in model.decode([next_token_id]) or "}" in model.decode([next_token_id]):
+                    dict_result += model.decode(next_token_id)
+                    llm_prompt += model.decode(next_token_id)
+                    break
+                dict_result += model.decode(next_token_id)
+                llm_prompt += model.decode(next_token_id)
 
         # print(functions_def_obj[int(function_number)].parameters[0].type)
         # print(llm_prompt, "\n\n")
         # print(function_number, "\n\n")
         # print(functions_def_list[int(function_number)], "\n\n")
-        # print(functions_def_list, "\n\n")
+        print(dict_result, "\n\n")
         return dict_result
     except Exception:
         sys.stderr.write("\033[91m")
