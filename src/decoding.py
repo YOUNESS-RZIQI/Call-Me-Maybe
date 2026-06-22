1f2ba1fd2d4b159509be6c51098a50ac672a6e16
YOUNESS-RZIQI <younessrziki204@gmail.com>
2026-06-22 17:14:49 +0100

    Updates

--- src/decoding.py
+++ src/decoding.py
@@ -8,6 +8,52 @@ from src.models import DefinitionValidator
 from src.parser import Parser
 
 
+def args_decoding(vc: dict, llm_prompt: str, dict_result: str, key_index: int, keys: List[str], ky_type: List[str]) -> tuple:
+            dict_result += '"' + keys[key_index] + '": '
+            llm_prompt += '"' + keys[key_index] + '": '
+            
+            if ky_type[key_index] in ("number", "decimal", "float", "num", "n"):
+                allowed_tokens_ids = {
+                vc["0"], vc["1"], vc["2"], vc["3"], vc["4"], vc["5"],
+                vc["6"], vc["7"], vc["8"], vc["9"], vc["."], vc[","],
+                vc["-"], vc["+"]}
+
+            if ky_type[key_index] in ("int", "integer"):
+                allowed_tokens_ids = {
+                vc["0"], vc["1"], vc["2"], vc["3"], vc["4"], vc["5"],
+                vc["6"], vc["7"], vc["8"], vc["9"], vc[","],
+                vc["-"], vc["+"]}
+
+            if ky_type[key_index] in ("bool", "boolean"):
+                allowed_tokens_ids = {vc["true"], vc["True"], vc["TRUE"],
+                                     vc["false"], vc["False"], vc["FALSE"],
+                                     vc[","]}
+
+            if ky_type[key_index] in ("string", "s", "str"):
+                allowed_tokens_ids = set(range(len(vc)))
+
+
+            # '",\n"parameters": { "a": '
+            
+                
+            while (1):
+                logits: List[float] = model.get_logits_from_input_ids(
+                    model.encode(llm_prompt)[0].tolist())
+                # masking the logits to only allow the allowed_tokens_ids.
+                for index in range(0, len(logits)):
+                    if index not in allowed_tokens_ids:
+                        logits[index] = float("-inf")
+    
+                next_token_id = int(np.argmax(logits))
+                if "," in model.decode([next_token_id]):
+                    dict_result += model.decode(next_token_id)
+                    llm_prompt += model.decode(next_token_id)
+                    break
+                dict_result += model.decode(next_token_id)
+                llm_prompt += model.decode(next_token_id)
+            return (llm_prompt, dict_result)
+
+
 def constrain_decoding(llm_prompt: str, input_prompt: str) -> str:
     """ Constrained Decoding for JSON """
     try:
@@ -66,50 +112,71 @@ def constrain_decoding(llm_prompt: str, input_prompt: str) -> str:
         llm_prompt += dict_result
 
         # i will do all the args until the last one and i will stop, and also i stop at comma ','
-        for key_index in range(0, len(keys)):
-            dict_result += '"' + keys[key_index] + '": '
-            llm_prompt += '"' + keys[key_index] + '": '
+        for key_index in range(0, len(keys) - 1):
+            llm_prompt, dict_result = args_decoding(vc,
+                                                    llm_prompt,
+                                                    dict_result,
+                                                    key_index,
+                                                    keys,
+                                                    ky_type)
+
+
+        # last arg:
+
+        last_key_index: int = len(keys) - 1
+        dict_result += '"' + keys[last_key_index] + '": '
+        llm_prompt += '"' + keys[last_key_index] + '": '
             
-            if ky_type[key_index] in ("number", "decimal", "float"):
-                allowed_tokens_ids = {
+        if ky_type[last_key_index] in ("number", "decimal", "float", "num", "n"):
+            allowed_tokens_ids = {
                 vc["0"], vc["1"], vc["2"], vc["3"], vc["4"], vc["5"],
-                vc["6"], vc["7"], vc["8"], vc["9"], vc["."], vc[","],
-                vc["-"], vc["+"]}
+                vc["6"], vc["7"], vc["8"], vc["9"], vc["."], vc["}"],
+                vc["-"], vc["+"], vc[","]}
 
-            if ky_type[key_index] in ("int", "integer"):
-                allowed_tokens_ids = {
+        if ky_type[last_key_index] in ("int", "integer"):
+            allowed_tokens_ids = {
                 vc["0"], vc["1"], vc["2"], vc["3"], vc["4"], vc["5"],
-                vc["6"], vc["7"], vc["8"], vc["9"], vc[","],
-                vc["-"], vc["+"]}
+                vc["6"], vc["7"], vc["8"], vc["9"], vc["}"],
+                vc["-"], vc["+"], vc[","]}
 
-            if ky_type[key_index] in ("bool", "boolean"):
-                allowed_tokens_ids = {vc["true"], vc["True"], vc["TRUE"],
+        if ky_type[last_key_index] in ("bool", "boolean"):
+            allowed_tokens_ids = {vc["true"], vc["True"], vc["TRUE"],
                                      vc["false"], vc["False"], vc["FALSE"],
-                                     vc[","]}
+                                     vc["}"], vc[","]}
 
-            if ky_type[key_index] in ("string", "s"):
-                allowed_tokens_ids = set(range(len(vc)))
+        if ky_type[last_key_index] in ("string", "s", "str"):
+            allowed_tokens_ids = set(range(len(vc)))
 
 
             # '",\n"parameters": { "a": '
             
                 
-            while (1):
-                logits: List[float] = model.get_logits_from_input_ids(
-                    model.encode(llm_prompt)[0].tolist())
-                # masking the logits to only allow the allowed_tokens_ids.
-                for index in range(0, len(logits)):
-                    if index not in allowed_tokens_ids:
-                        logits[index] = float("-inf")
+        while (1):
+            logits: List[float] = model.get_logits_from_input_ids(
+                model.encode(llm_prompt)[0].tolist())
+            # masking the logits to only allow the allowed_tokens_ids.
+            for index in range(0, len(logits)):
+                if index not in allowed_tokens_ids:
+                    logits[index] = float("-inf")
     
-                next_token_id = int(np.argmax(logits))
-                if "," in model.decode([next_token_id]) or "}" in model.decode([next_token_id]):
-                    dict_result += model.decode(next_token_id)
-                    llm_prompt += model.decode(next_token_id)
-                    break
-                dict_result += model.decode(next_token_id)
-                llm_prompt += model.decode(next_token_id)
+            next_token_id = int(np.argmax(logits))
+            if ky_type[last_key_index] in ("bool", "boolean") and model.decode([next_token_id]) not in ("bool", "boolean"):
+                dict_result += "}"
+                llm_prompt += "}"
+                break
+            elif ky_type[last_key_index] in ("string", "s", "str") and model.decode([next_token_id]) not in ('"', "}"):
+                dict_result += "}"
+                llm_prompt += "}"
+                break
 
+            elif ky_type[last_key_index] in ("number", "decimal", "float", "num", "n", "int", "integer") and not model.decode([next_token_id]).isdigit() and model.decode([next_token_id]) not in (".-+"):
+                dict_result += "}"
+                llm_prompt += "}"
+                break
+
+
+            dict_result += model.decode(next_token_id)
+            llm_prompt += model.decode(next_token_id)
         # print(functions_def_obj[int(function_number)].parameters[0].type)
         # print(llm_prompt, "\n\n")
         # print(function_number, "\n\n")
